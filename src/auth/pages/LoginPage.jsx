d7dbd8f1980e2433d5a630f063b719462a9e4292
Beto <ivjalberto@gmail.com>
2023-08-15 15:41:06 -0600

    Clase 243

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,58 @@
+import { Grid, TextField, Typography } from "@mui/material"
+import { borderRadius, padding } from "@mui/system"
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Grid 
+      container
+      spacing={ 0 }
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: 'primary.main',
+        padding: 4
+      }}
+    >
+
+      <Grid item
+        className="box-shadow"
+        xs={ 3 }
+        sx={{
+          backgroundColor: 'white',
+          padding: 3,
+          borderRadius: 2
+        }}
+      >
+        <Typography variant='h5' sx={ {mb:1 }}>Login</Typography>
+
+        <form >
+          <Grid container>
+            <Grid item xs={12} sx={ {mt: 2} }>
+              <TextField 
+                label="Correo" 
+                type="email" 
+                placeholder='Correo@google.com'
+                fullWidth
+              /> 
+            </Grid>
+
+            <Grid item xs={12} sx={ {mt: 1} }>
+              <TextField 
+                label="Contrasenha" 
+                type="password" 
+                placeholder='Contrasenha'
+                fullWidth
+              /> 
+            </Grid>
+
+          </Grid>
+        </form>
+
+
+      </Grid>
+
+    </Grid>
   )
 }
