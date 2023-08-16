d9fcb5785ce639901be44c5acddffec78b8a1bcd
IVJAlberto <ivjalberto@gmail.com>
2023-08-16 11:35:09 -0600

    Clase 248

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,13 +1,13 @@
 import { Box } from '@mui/system'
-
+import { NavBar } from "../components/NavBar";
 
 const drawerWidth = 240;
 
 export const JournalLayout = ( { children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-        {/* NavBar */}
-
+        
+        <NavBar/>
         {/* Sidebar */}
 
         <Box 
