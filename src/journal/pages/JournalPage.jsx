4f96c28a61c8f852391ee09c433feac59689f9d8
IVJAlberto <ivjalberto@gmail.com>
2023-08-16 11:07:03 -0600

    Clase 247

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-    <Typography variant="h1" >JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography >Et sint pariatur tempor consectetur laboris tempor do id id aliquip consectetur anim quis ex. Anim consequat duis laboris aliqua elit amet aliquip deserunt labore tempor enim. Irure id proident sit sit deserunt occaecat cupidatat commodo ut sunt. Lorem nulla in occaecat incididunt ex eiusmod ipsum reprehenderit. Laboris pariatur enim magna nisi nisi magna nostrud.</Typography>
+    </JournalLayout>
   )
 }
