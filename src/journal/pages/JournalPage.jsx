c78fd416a3e7e2622f6b5aed079a892c42212e6d
Beto <ivjalberto@gmail.com>
2023-08-17 16:17:45 -0600

    Clase 253

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
@@ -7,8 +8,22 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography >Et sint pariatur tempor consectetur laboris tempor do id id aliquip consectetur anim quis ex. Anim consequat duis laboris aliqua elit amet aliquip deserunt labore tempor enim. Irure id proident sit sit deserunt occaecat cupidatat commodo ut sunt. Lorem nulla in occaecat incididunt ex eiusmod ipsum reprehenderit. Laboris pariatur enim magna nisi nisi magna nostrud.</Typography> */}
-      {/* <NothingSelectedView/> */}
+      {/* {<NothingSelectedView/>} */}
       <NoteView/>
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main ', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
