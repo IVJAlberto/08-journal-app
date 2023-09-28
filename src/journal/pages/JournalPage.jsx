c76c6f27939ff9004917f7675eb154f279a93c36
Beto <ivjalberto@gmail.com>
2023-09-28 15:20:17 -0600

    Clase 275

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -7,6 +7,7 @@ import { NothingSelectedView } from "../views/NothingSelectedView"
 export const JournalPage = () => {
   return (
     <JournalLayout>
+      
       {/* <Typography >Et sint pariatur tempor consectetur laboris tempor do id id aliquip consectetur anim quis ex. Anim consequat duis laboris aliqua elit amet aliquip deserunt labore tempor enim. Irure id proident sit sit deserunt occaecat cupidatat commodo ut sunt. Lorem nulla in occaecat incididunt ex eiusmod ipsum reprehenderit. Laboris pariatur enim magna nisi nisi magna nostrud.</Typography> */}
       {/* {<NothingSelectedView/>} */}
       <NoteView/>
