585b1d0818d6063368e626f6dc393327675b54bc
Beto <ivjalberto@gmail.com>
2023-10-24 16:57:24 -0600

    Clase 294

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -37,7 +37,10 @@ export const LoginPage = () => {
   return (
         <AuthLayout titulo='login'>
 
-          <form onSubmit={ onSubmit }>
+          <form 
+            className='animate__animated animate__fadeIn animate__faster'
+            onSubmit={ onSubmit }
+          >
             <Grid container>
               <Grid item xs={12} sx={ {mt: 2} }>
                 <TextField 
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -48,6 +48,7 @@ export const RegisterPage = () => {
         <AuthLayout titulo='Crear cuenta'>
           <h1>FormValid { isFormValid ? 'Valido' : 'Incorrecto'}</h1>
           <form 
+            className='animate__animated animate__fadeIn animate__faster'
             onSubmit={ onSubmit }
           >
             <Grid container>
