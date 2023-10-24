30b67efde59e8d1015386f19d5a727cb48fd8ad8
Beto <ivjalberto@gmail.com>
2023-10-24 13:23:28 -0600

    Clase 282 y 283 sin errores

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
 
+import { useMemo, useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout'
@@ -11,9 +12,20 @@ const formData = {
     displayName: 'Fluke Ario'
 }
 
+const formValidations = {
+  email: [ (value) => value.includes('@'), 'El correo debe de tener una @'],
+  password: [ (value) => value.length >= 6, 'El password debe de tener más de 6 letras.'],
+  displayName: [ (value) => value.length >= 1, 'El nombre es obligatorio.'],
+}
+
+
 export const RegisterPage = () => {
 
-  const { displayName,email, password, onInputChange} = useForm(formData);
+  const { formState, displayName,email, password, onInputChange,
+          isFormValid, displayNameValid, emailValid, passwordValid, 
+  } = useForm(formData, formValidations );
+
+  // console.log( displayNameValid );
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -35,6 +47,8 @@ export const RegisterPage = () => {
                   name='displayName'
                   value={ displayName }
                   onChange={ onInputChange }
+                  error={ !displayNameValid }
+                  helperText={ displayNameValid }
                 /> 
               </Grid>
 
@@ -52,14 +66,16 @@ export const RegisterPage = () => {
 
               <Grid item xs={12} sx={ {mt: 1} }>
                 <TextField 
-                  label="Contrasenha" 
+                  label="Contraseña" 
                   type="password" 
-                  placeholder='Contrasenha'
+                  placeholder='Contraseña' 
                   fullWidth
-                  name='contrasena'
+                  name="password"
                   value={ password }
                   onChange={ onInputChange }
-                /> 
+                  // error={ !!passwordValid && formSubmitted  }
+                  helperText={ passwordValid }
+                />
               </Grid>
 
               <Grid container spacing={2} sx={{ mt: 1 , mb: 2}}>
