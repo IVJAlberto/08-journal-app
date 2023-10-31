7f5022dd5619fc3be6883b3e4d0711861a23b63f
Beto <ivjalberto@gmail.com>
2023-10-31 14:08:20 -0600

    Clase 301

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,8 +3,17 @@ import { AddOutlined } from "@mui/icons-material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
+import { startNewNote } from "../../store/journal/thunks"
+import { useDispatch } from "react-redux"
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  }
+
   return (
     <JournalLayout>
       
@@ -13,6 +22,7 @@ export const JournalPage = () => {
       {/* <NoteView/> */}
 
       <IconButton
+        onClick={ onClickNewNote }
         size="large"
         sx={{
           color: 'white',
