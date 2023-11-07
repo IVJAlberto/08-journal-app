ad77026648c792eb122e5b521a9f39370761d43b
IVJAlberto <ivjalberto@gmail.com>
2023-11-06 19:12:16 -0600

    Clase 302

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -4,11 +4,13 @@ import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 import { startNewNote } from "../../store/journal/thunks"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { useMemo } from 'react'
 
 export const JournalPage = () => {
 
   const dispatch = useDispatch();
+  const { isSaving, active } = useSelector( state => state.journal );
 
   const onClickNewNote = () => {
     dispatch(startNewNote());
@@ -18,10 +20,16 @@ export const JournalPage = () => {
     <JournalLayout>
       
       {/* <Typography >Et sint pariatur tempor consectetur laboris tempor do id id aliquip consectetur anim quis ex. Anim consequat duis laboris aliqua elit amet aliquip deserunt labore tempor enim. Irure id proident sit sit deserunt occaecat cupidatat commodo ut sunt. Lorem nulla in occaecat incididunt ex eiusmod ipsum reprehenderit. Laboris pariatur enim magna nisi nisi magna nostrud.</Typography> */}
-      {<NothingSelectedView/>}
-      {/* <NoteView/> */}
+      {
+        ( !!active ) 
+        ?  <NoteView/>
+        : <NothingSelectedView/>
+      }
+      
+        
 
       <IconButton
+        disabled= { isSaving }
         onClick={ onClickNewNote }
         size="large"
         sx={{
--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const journalSlice = createSlice({
     name: 'journal',
     initialState: {
-        isSaving: true,
+        isSaving: false,
         messageSaves: '',
         notes: [],
         active: null,
@@ -18,11 +18,15 @@ export const journalSlice = createSlice({
 
     },
     reducers: {
+        savingNewNote: ( state ) => {
+            state.isSaving = true;
+        },
         addNewEmptyNote: (state, action) => {
-
+            state.notes.push( action.payload );
+            state.isSaving = false;
         },
         setActiveNote: (state, action) => {
-
+            state.active = action.payload;
         },
         setNotes: (state, action) => {
 
@@ -42,6 +46,7 @@ export const journalSlice = createSlice({
 
 // Action creators are generated for each case reducer function
 export const { 
+    savingNewNote,
     addNewEmptyNote,
     setActiveNote,
     setNotes,
