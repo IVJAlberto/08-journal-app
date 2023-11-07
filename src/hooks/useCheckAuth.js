8f798e992aac10eb3b991e244add1484b83efbe2
IVJAlberto <ivjalberto@gmail.com>
2023-11-06 20:19:25 -0600

    Clase 303

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FirebaseAuth } from "../firebase/config";
 import { login, logOut } from "../store/auth";
+import { startLoadingNotes } from "../store/journal";
 
 export const useCheckAuth = () => {
   
@@ -13,8 +14,10 @@ export const useCheckAuth = () => {
         
         onAuthStateChanged( FirebaseAuth, async( user ) => {
             if( !user ) return dispatch( logOut() );
+
             const { uid, email, displayName, photoURL} = user;
             dispatch( login({uid, email, displayName, photoURL}) );
+            dispatch( startLoadingNotes() );
         } )
         
     }, [])
