224f09140225e65446684310aae5ddf77a59a3b1
Beto <ivjalberto@gmail.com>
2023-11-09 13:42:27 -0600

    Clase 311

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { SaveOutlined } from "@mui/icons-material"
-import { Button, Grid, TextField, Typography } from "@mui/material"
+import { SaveOutlined, UploadOutlined } from "@mui/icons-material"
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material"
 import Swal from 'sweetalert2/dist/sweetalert2.js'
 import 'sweetalert2/src/sweetalert2.scss'
 
@@ -18,7 +18,9 @@ export const NoteView = () => {
     const dateString = useMemo(() => {
         const newDate = new Date(date);
         return newDate.toUTCString();
-    }, [date])
+    }, [date]);
+
+    const fileInputRef = useRef(); 
 
     useEffect(() => {
       dispatch( setActiveNote(formState) );    
@@ -37,10 +39,33 @@ export const NoteView = () => {
         dispatch( startSaveNote() );
     }
 
+    const onFileInputChange = ({ target }) =>{
+        if(target.files ===0 ) return;
+
+        // dispatch( starUploadingFiles(target.files ));
+
+    }
+
     return (
         <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1}}>
             <Grid item>
                 <Typography fontSize={39} fontWeight='light'> {dateString}</Typography>
+            </Grid>
+            <Grid item>
+                <input 
+                    type="file" 
+                    multiple
+                    ref={ fileInputRef }
+                    onChange={ onFileInputChange }
+                    style={{ display: 'none' }}
+                />
+                <IconButton 
+                    color="primary"
+                    disabled={ isSaving }
+                    onClick={ () => fileInputRef.current.click() }
+                >
+                    <UploadOutlined  sx={ { fontSize:30, mr:1 }}/>
+                </IconButton>
             </Grid>
             <Grid item>
                 <Button 
